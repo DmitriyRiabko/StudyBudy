3457e121260a3feaa0d97167853ee40eab535c76
vsegda_vinovat <riabko97@gmail.com>
2024-01-07 00:01:22 +0200

    added view for create-room

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 # rooms = [
     
@@ -20,3 +21,17 @@ def room(request,pk):
             
     context = {'room':room}
     return render(request, 'base/room.html' ,context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    context = {'form':form}
+    
+    return render(request,'base/room_form.html',context)
