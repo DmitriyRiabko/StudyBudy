9a8906baac5f0a3f07ef37c2c1f5d94dcfafc9dc
vsegda_vinovat <riabko97@gmail.com>
2024-01-07 17:13:09 +0200

    added filter feature for topics

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 # rooms = [
@@ -11,8 +11,11 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()    
-    context = {'rooms':rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name__contains=q)    
+    topics = Topic.objects.all()
+    context = {'rooms':rooms, 
+               'topics':topics}
     return render(request, 'base/home.html', context)
 
 
@@ -49,3 +52,13 @@ def updateRoom(request,pk):
     
     context = {'form':form}
     return render(request, 'base/room_form.html',context)
+
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id =pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect('home')
+        
+    return render(request,'base/delete.html',{'obj':room})
