26b22b3baddf7e16bc10269f637dfc03af8c4afd
vsegda_vinovat <riabko97@gmail.com>
2024-01-08 01:39:52 +0200

    added new urls

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,8 @@ from .import views
 urlpatterns = [
     path('login/',views.loginPage, name='login'),
     path('logout/',views.logoutUser, name='logout'),
+    path('register/',views.registerUser, name='register'),
+    
     
     
     path('', views.home, name='home'),
